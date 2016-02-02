5128fc71f5f0c294c2078a730e82ebb7da39964c
Joseph R Dowdle <joedowdle@gmail.com>
2016-02-01 20:05:26 -0600

    Completed Course: DEV204x_Programming_w_C#

---
+++ DEV204x_Programming_w_C#/Module_02/Assignment_02/Assignment_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Assignment_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string toPrint = "";
+            bool flip = true;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (flip)
+                    {
+                        toPrint += "X";
+                    }
+                    else
+                    {
+                        toPrint += "O";
+                    }
+                    flip = !flip;
+                }
+                Console.WriteLine(toPrint);
+                toPrint = "";
+                flip = !flip;
+            }
+        }
+    }
+}
---
+++ DEV204x_Programming_w_C#/Module_09/Mod_9_Homework/Mod_9_Homework/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System;
+
+namespace Mod_9_Homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Student> Students { get; set; }
+
+        private static int currentStudent = 0;
+
+        public MainWindow()
+        {
+            Students = new List<Student>();
+
+            InitializeComponent();
+        }
+
+        private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student();
+
+            // set student information
+            student.FirstName = txtFirstName.Text;
+            student.LastName = txtLastName.Text;
+            student.City = txtCity.Text;
+
+            // add student to list
+            Students.Add(student);
+
+            // clear text boxes
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtCity.Clear();
+
+            // reset list location
+            currentStudent = 0;
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PopulateStudentText(Students[currentStudent]);
+                currentStudent--;
+                if (currentStudent < 0)
+                {
+                    currentStudent = Students.Count - 1;
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("No students in list.");
+            }            
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PopulateStudentText(Students[currentStudent]);
+                currentStudent++;
+                if (currentStudent > Students.Count - 1)
+                {
+                    currentStudent = 0;
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("No students in list.");
+            }            
+        }
+
+        private void PopulateStudentText(Student student)
+        {
+            txtFirstName.Text = student.FirstName;
+            txtLastName.Text = student.LastName;
+            txtCity.Text = student.City;
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }        
+
+        public Student()
+        {
+
+        }
+    }
+}
